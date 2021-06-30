b21821d49e42109f8670b0b73944fd3322d34020
Luca Parisi <71427921+luca-parisi@users.noreply.github.com>
2021-06-30 23:18:41 +0100

    Improved readability of QReLU_TensorFlow_Keras.py
    Improved readability and facilitated direct reusability of QReLU_TensorFlow_Keras.py

--- QReLU_TensorFlow_Keras.py
+++ QReLU_TensorFlow_Keras.py
@@ -1,7 +1,29 @@
-# QReLU as a custom activation function in TensorFlow
+"""Additional utility for Deep Learning models in TensorFlow and Keras"""
+
+# The Quantum ReLU or 'QReLU' as a custom activation function in TensorFlow (tf_q_relu)
+# and Keras (QReLU)
+
+# Author: Luca Parisi <luca.parisi@ieee.org>
+
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+
+# QReLU as a custom activation function in TensorFlow
+
+'''
+# Example of usage of the QReLU in TensorFlow as a custom activation function of a convolutional layer (#2)
+
+convolutional_layer_2 = tf.layers.conv2d(
+                        inputs=pooling_layer_1,
+                        filters=64,
+                        kernel_size=[5, 5],
+                        padding="same")
+convolutional_layer_activation = tf_q_relu(convolutional_layer_2)
+pooling_layer_2 = tf.layers.max_pooling2d(inputs=convolutional_layer_activation, pool_size=[2, 2], strides=2)
+'''
 
 # Defining the QReLU function
 def q_relu(x):
@@ -42,6 +64,7 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
         return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
 
 np_q_relu_32 = lambda x: np_q_relu(x).astype(np.float32)
+
 def tf_q_relu(x,name=None):
     with tf.name_scope(name, "q_relu", [x]) as name:
         y = py_func(np_q_relu_32,   # Forward pass function
@@ -51,7 +74,9 @@ def tf_q_relu(x,name=None):
                          grad= q_relu_grad) # The function that overrides gradient
         y[0].set_shape(x.get_shape())     # To specify the rank of the input.
         return y[0]
+
 np_d_q_relu_32 = lambda x: np_d_q_relu(x).astype(np.float32)
+
 def tf_d_q_relu(x,name=None):
     with tf.name_scope(name, "d_q_relu", [x]) as name:
         y = tf.py_func(np_d_q_relu_32,
@@ -61,17 +86,17 @@ def tf_d_q_relu(x,name=None):
                         stateful=False)
         return y[0]
 
-# Example of usage in TensorFlow with a QReLU layer between a convolutional layer (#2) and a pooling layer (#2)
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,
-      kernel_size=[5, 5],
-      padding="same")
-  conv2_act = tf_q_relu(conv2)
-  pool2 = tf.layers.max_pooling2d(inputs=conv2_act, pool_size=[2, 2], strides=2)
-  
+
 # QReLU as a custom layer in Keras 
-from tensorflow.keras.layers import Layer
+
+'''
+# Example of usage of the QReLU as a Keras layer in a sequential model between a convolutional layer and a pooling layer
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+model.add(QReLU())
+model.add(layers.MaxPooling2D((2, 2)))
+'''
 
 class QReLU(Layer):
 
@@ -90,10 +115,3 @@ class QReLU(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape
-
-# Example of usage in a sequential model in Keras with a QReLU layer between a convolutional layer and a pool-ing layer
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
-model.add(QReLU())
-model.add(layers.MaxPooling2D((2, 2)))
