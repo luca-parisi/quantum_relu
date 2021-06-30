b87ba04cfaf444c78004d92bc4dfa58243a57c2e
Luca Parisi <71427921+luca-parisi@users.noreply.github.com>
2021-06-30 23:27:22 +0100

    Improved readability of m_QReLU_TensorFlow_Keras.py
    Improved readability and facilitated direct reusability of m_QReLU_TensorFlow_Keras.py

--- m_QReLU_TensorFlow_Keras.py
+++ m_QReLU_TensorFlow_Keras.py
@@ -1,6 +1,31 @@
+"""Additional utility for Deep Learning models in TensorFlow and Keras"""
+
+# The modified Quantum ReLU or 'm-QReLU' as a custom activation function in TensorFlow (tf_m_q_relu)
+# and Keras (m_QReLU)
+
+# Author: Luca Parisi <luca.parisi@ieee.org>
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+
 # m-QReLU as a custom activation function in TensorFlow
 
-# Defining the m-QReLU (modified QReLU) function
+'''
+# Example of usage of the m-QReLU in TensorFlow as a custom activation function of a convolutional layer (#2)
+
+convolutional_layer_2 = tf.layers.conv2d(
+                        inputs=pooling_layer_1,
+                        filters=64,
+                        kernel_size=[5, 5],
+                        padding="same")
+convolutional_layer_activation = tf_m_q_relu(convolutional_layer_2)
+pooling_layer_2 = tf.layers.max_pooling2d(inputs=convolutional_layer_activation, pool_size=[2, 2], strides=2)
+'''
+
+# Defining the m-QReLU function
 def m_q_relu(x):
   if x>0:
     x = x
@@ -12,7 +37,7 @@ def m_q_relu(x):
 # Vectorising the m-QReLU function  
 np_m_q_relu = np.vectorize(m_q_relu)
 
-# Defining the derivative of the function QReLU
+# Defining the derivative of the function m-QReLU
 def d_m_q_relu(x):
   if x>0:
     x = 1
@@ -24,13 +49,22 @@ def d_m_q_relu(x):
 # Vectorising the derivative of the m-QReLU function  
 np_d_m_q_relu = np.vectorize(d_m_q_relu)
 
-# Defining the gradient function of the QReLU
+# Defining the gradient function of the m-QReLU
 def m_q_relu_grad(op, grad):
     x = op.inputs[0]
     n_gr = tf_d_m_q_relu(x)
     return grad * n_gr
 
+def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
+# Generating a unique name to avoid duplicates:
+    rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+2))
+    tf.RegisterGradient(rnd_name)(grad)
+    g = tf.get_default_graph()
+    with g.gradient_override_map({"PyFunc": rnd_name}):
+        return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
+  
 np_m_q_relu_32 = lambda x: np_m_q_relu(x).astype(np.float32)
+
 def tf_m_q_relu(x,name=None):
     with tf.name_scope(name, "m_q_relu", [x]) as name:
         y = py_func(np_m_q_relu_32,   # Forward pass function
@@ -40,7 +74,9 @@ def tf_m_q_relu(x,name=None):
                          grad= m_q_relu_grad) # The function that overrides gradient
         y[0].set_shape(x.get_shape())     # To specify the rank of the input
         return y[0]
+
 np_d_m_q_relu_32 = lambda x: np_d_m_q_relu(x).astype(np.float32)
+
 def tf_d_m_q_relu(x,name=None):
     with tf.name_scope(name, "d_m_q_relu", [x]) as name:
         y = tf.py_func(np_d_m_q_relu_32,
@@ -50,17 +86,17 @@ def tf_d_m_q_relu(x,name=None):
                         stateful=False)
         return y[0]
 
-# Example of usage in TensorFlow with a m-QReLU layer between a convolutional layer (#2) and a pooling layer (#2)
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,
-      kernel_size=[5, 5],
-      padding="same")
-  conv2_act = tf_m_q_relu(conv2)
-  pool2 = tf.layers.max_pooling2d(inputs=conv2_act, pool_size=[2, 2], strides=2)
-  
+
 # m-QReLU as a custom layer in Keras 
-from tensorflow.keras.layers import Layer
+
+'''
+# Example of usage of the m-QReLU as a Keras layer in a sequential model between a convolutional layer and a pooling layer
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+model.add(m_QReLU())
+model.add(layers.MaxPooling2D((2, 2)))
+'''
 
 class m_QReLU(Layer):
 
@@ -79,11 +115,3 @@ class m_QReLU(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape
-
-# Example of usage in a sequential model in Keras with a m-QReLU layer between a convolutional layer and a pool-ing layer
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
-#model.add(QReLU())
-model.add(m_QReLU())
-model.add(layers.MaxPooling2D((2, 2)))
