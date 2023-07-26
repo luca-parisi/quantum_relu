09bb72d4d6fc69689db8c08487f11758e36c5df4
Luca Parisi <luca.parisi@ieee.org>
2023-07-26 18:38:50 +0100

    Simplify cnn_example.py and decouple the model creation into the function create_model

--- src/examples/tf_keras/cnn_example.py
+++ src/examples/tf_keras/cnn_example.py
@@ -6,87 +6,67 @@ Adapted from https://keras.io/examples/vision/mnist_convnet/
 """
 
 import numpy as np
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras import layers
 
 from tf_keras.constants import USE_M_QRELU
 from tf_keras.quantum_activations import QuantumReLU
 
-# Use the m-QReLU in the two convolutional layers of a CNN if modified = True, otherwise use the QReLU (by default,
-# as modified = False).
-modified = USE_M_QRELU
 
-"""
-## Prepare the data
-"""
-
-# Model / data parameters
 num_classes = 10
 inputs_shape = (28, 28, 1)
 
-# Load the data and split it between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
+# Scale images to the [0, 1] range and reshape
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
-
 
 # Convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-"""
-## Build the model
-"""
 
-model = keras.Sequential(
-    [
-        keras.Input(shape=inputs_shape),
+def create_model(modified: bool = False) -> tf.keras.Model:
+    """
+    Create a convolutional neural network model with the QuantumReLU activation.
 
-        # First convolutional layer with the 'QuantumReLU' activation function
+    Args:
+        modified: bool, optional
+            Whether to use the modified version of the QReLU or m-QReLU (False by default, i.e., using the QReLU).
+
+    Returns:
+        tf.keras.Model
+            The constructed Keras model with QuantumReLU activations.
+    """
+
+    sequential_model = tf.keras.Sequential([
+        tf.keras.Input(shape=inputs_shape),
         layers.Conv2D(32, kernel_size=(3, 3)),
-        # Instead of layers.Conv2D(64, kernel_size=(3, 3), activation="relu") when using the ReLU activation
         QuantumReLU(modified=modified),
-
         layers.MaxPooling2D(pool_size=(2, 2)),
-
-        # Second convolutional layer with the 'QuantumReLU' activation function
         layers.Conv2D(64, kernel_size=(3, 3)),
         QuantumReLU(modified=modified),
-
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Flatten(),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation="softmax"),
-    ]
-)
+    ])
+    return sequential_model
 
+
+model = create_model(modified=USE_M_QRELU)
 model.summary()
 
-"""
-## Train the model
-"""
-
+# Train the model
 batch_size = 128
 epochs = 2
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
-model.compile(loss="categorical_crossentropy",
-              optimizer="adam", metrics=["accuracy"])
-
-model.fit(x_train, y_train, batch_size=batch_size,
-          epochs=epochs, validation_split=0.1)
-
-"""
-## Evaluate the trained model
-"""
-
+# Evaluate the trained model
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
